72657bb1898c31a878f47fffeaaa854a73c3c3ed
EscapESC <radavasik@gmail.com>
2023-12-02 12:10:08 +0100

    Still trying to get is to compile

---
+++ Engine/Engine.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "SDL.h"
+
+int main(int argc, char *argv[]){
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("X-Base Engine",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 800,600, SDL_WINDOW_SHOWN);
+
+    return 0;
+}
