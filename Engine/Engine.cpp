f94851614175af7365ffb7319c1a0b33c59bfe4e
EscapESC <radavasik@gmail.com>
2023-12-02 12:10:22 +0100

    up

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include "SDL.h"
+#include "include\SDL.h"
 
 int main(int argc, char *argv[]){
-
+    
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow("X-Base Engine",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 800,600, SDL_WINDOW_SHOWN);
 
