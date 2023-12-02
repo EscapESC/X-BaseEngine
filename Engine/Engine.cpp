cd02342771fff9fc0d4febfa7c3c70f924101088
EscapESC <radavasik@gmail.com>
2023-12-02 12:59:47 +0100

    up

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]){
     
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("X-Base Engine",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 800,600, SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("Window",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 800,600, SDL_WINDOW_SHOWN);
 
     return 0;
 }
