bff6a32ce9b621be7192edfa2bedafc7a9126750
EscapESC <radavasik@gmail.com>
2023-12-03 15:41:30 +0100

    THE HOLY TRIANGLE

--- main.cpp
+++ main.cpp
@@ -27,6 +27,18 @@ int main(int argc, char *argv[]){
 
     gp.Xwindow_clear(renderer,0,0,0,0);
     gp.XdrawImage(Lambda, renderer,nullptr, prect);
+
+    SDL_Vertex vertex_1 = {{100.5, 200.5}, {255, 0, 0, 255}, {1, 1}};
+    SDL_Vertex vertex_2 = {{200.5, 100.5}, {0, 255, 0, 255}, {1, 1}};
+    SDL_Vertex vertex_3 = {{300.5, 200.5}, {0, 0, 255, 255}, {1, 1}};
+
+    SDL_Vertex vertices[] = {
+        vertex_2,
+        vertex_1,
+        vertex_3
+    };
+
+    SDL_RenderGeometry(renderer, NULL,vertices, 3, NULL, 0);
     gp.Xpaint(renderer);
     
     SDL_Delay(10000);
